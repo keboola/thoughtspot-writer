fb6bb1201f704e1b91d912b1ed753583f1593f5a
Ondra Jodas <ondrej.jodas@keboola.com>
2020-02-26 13:40:40 +0100

    exceptions for database and schema does not exists

---
+++ src/Keboola/ThoughtSpot/Exception/SchemaNotExistsException.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Keboola\ThoughtSpot\Exception;
+
+use Keboola\DbWriter\Exception\UserException;
+
+class SchemaNotExistsException extends UserException
+{
+}
